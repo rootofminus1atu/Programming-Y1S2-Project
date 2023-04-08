5cd6a2611402382bd68e9cd7fe263d7997190f9d
s00240122 <s00240122@atu.ie>
2023-04-08 23:58:16 +0100

    1st and 2nd part done
    using linq array functions and object destructuring

---
+++ Csharp-console-app/Csharp-console-app/Passenger.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Csharp_console_app
+{
+    public class Passenger
+    {
+        public string LastName { get; set; }
+        public string FirstName { get; set; }
+        public double Age { get; set; }
+        public string Sex { get; set; }
+        public string Occupation { get; set; }
+        public string NativeCountry { get; set; }
+        public Ship Ship { get; set; }
+
+
+        public Passenger() { }
+
+        public Passenger(string lastName, string firstName, double age, string sex, string occupation, string nativeCountry, Ship ship)
+        {
+            LastName = lastName;
+            FirstName = firstName;
+            Age = age;
+            Sex = sex;
+            Occupation = occupation;
+            NativeCountry = nativeCountry;
+            Ship = ship;
+        }
+
+        public override string ToString()
+        {
+            return $"{this.LastName} {this.FirstName} aged {this.Age}";
+        }
+    }
+
+    public static class PassengerExtension
+    {
+        // the implementation below gets only a list of unique ships, it tells us nothing about how many passengers there are on each
+        public static List<Ship> GetShips(this List<Passenger> passengers)
+        {
+            List<Ship> ships = passengers
+                .Select(x => x.Ship)
+                .GroupBy(x => x.ShipID)  
+                .Select(x => x.First())  // this is wacky but it works
+                .ToList();
+
+            return ships;
+        }
+
+        // questionable implementation
+        public static List<Ship> GetShips2(this List<Passenger> passengers)
+        {
+            List<Ship> uniqueShips = new List<Ship>() { };
+
+            foreach (Passenger passenger in passengers)
+            {
+                Ship ship = uniqueShips.Find(s => s.ShipID == passenger.Ship.ShipID);
+
+                if (ship == null)
+                {
+                    ship = new Ship(
+                        passenger.Ship.DestinationCountry,
+                        passenger.Ship.DepartureSeaport,
+                        passenger.Ship.ShipID,
+                        passenger.Ship.ArrivalDate
+                        );
+
+                    uniqueShips.Add(ship);
+                }
+
+                ship.Count++;
+            }
+
+            return uniqueShips;
+        }
+
+        public static List<Passenger> GetPassengersOnShip(this List<Passenger> passengers, Ship ship)
+        {
+            List<Passenger> theChosenOnes = passengers
+                .Where(p => p.Ship.ShipID == ship.ShipID)
+                .ToList();
+
+            return theChosenOnes;
+
+            // this could be used to get the count too! pretty easily
+            // instead of typing
+            // p.Ship.ShipID == ship.ShipID
+            // I could implement ship equality
+        }
+    
+        public static List<(string, int)> GetOccupations(this List<Passenger> passengers)
+        {
+            List<(string, int)> occupations = passengers
+                .Select(p => p.Occupation)
+                .GroupBy(x => x)
+                .Select(x => (x.Key, x.Count()))
+                .ToList();
+
+            return occupations;
+        }
+    }
+
+    public class Ship
+    {
+        public string DestinationCountry { get; set; }
+        public string DepartureSeaport { get; set; }
+        public string ShipID { get; set; }
+        public string ArrivalDate { get; set; }
+        public int Count { get; set; }
+
+        public Ship(string destinationCountry, string departureSeaport, string shipID, string arrivalDate)
+        {
+            DestinationCountry = destinationCountry;
+            DepartureSeaport = departureSeaport;
+            ShipID = shipID;
+            ArrivalDate = arrivalDate;
+            // no Count assignment
+        }
+
+    }
+
+    public class Report
+    {
+        public List<Passenger> Passengers { get; set; }
+
+        public Report(List<Passenger> passengers)
+        {
+            Passengers = passengers;
+        }
+    }
+
+}
--- Csharp-console-app/Csharp-console-app/Program.cs
+++ Csharp-console-app/Csharp-console-app/Program.cs
@@ -1,10 +1,113 @@
-﻿namespace Csharp_console_app
+﻿using System.Text.RegularExpressions;
+
+namespace Csharp_console_app
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string filePath = @"../../../../../faminefiletoanalyse.csv";
+
+            List<Passenger> passengerList = GetPassengerData(filePath);
+
+
+
+
+            List<Ship> ships = passengerList.GetShips();
+
+            foreach (Ship ship in ships)
+                Console.WriteLine($"{ship.ShipID} arrived on {ship.ArrivalDate} WITH COUNT {ship.Count}");
+
+
+            List<Passenger> filtered = passengerList.GetPassengersOnShip(ships[1]);
+
+            Console.WriteLine($"{ships[1].ShipID} leaving from {ships[1].DepartureSeaport}\nArrived: {ships[1].ArrivalDate} with {filtered.Count} passengers");
+
+            foreach(Passenger passenger in filtered)
+                Console.WriteLine(passenger.ToString());
+
+            Console.WriteLine(filtered.Count);
+
+
+
+            List<(string, int)> occupations = passengerList.GetOccupations();
+
+            foreach(var item in occupations)
+            {
+                var (occupation, count) = item;
+                Console.WriteLine($"uhh {occupation} and {count}");
+            }
+
+        }
+
+        // this below is ugly 
+        // fix later
+        static double AgeParse(string ageString)
+        {
+            string pattern = @"\d+";
+
+
+            if (ageString.StartsWith("age"))
+            {
+                Match match = Regex.Match(ageString, pattern);
+                double number = double.Parse(match.Value);
+                return number;
+            }
+
+
+            else if (ageString.StartsWith("Infant"))
+            {
+                Match match = Regex.Match(ageString, pattern);
+                double number = double.Parse(match.Value);
+                return number / 12;
+            }
+
+
+            return 0;
+        }
+
+        static List<Passenger> GetPassengerData(string path)
+        {
+            List<Passenger> passengers = new List<Passenger>();
+
+            using (StreamReader sr = File.OpenText(path))
+            {
+                // read and discard the first line
+                sr.ReadLine();
+
+                string? s;
+                while ((s = sr.ReadLine()) != null)
+                {
+                    string[] fields = s.Split(",");
+
+                    Passenger passenger = new()  // wack syntax
+                    {
+                        // the current implementation depends on the correct ordering of the columns
+                        // it's possible to make it depend on the header row names
+                        // but I'm going with the former, since changing the header row is easier than rearranging the columns
+
+                        LastName = fields[0],
+                        FirstName = fields[1],
+                        Age = AgeParse(fields[2]),
+                        Sex = fields[3],
+                        Occupation = fields[4],
+                        NativeCountry = fields[5],
+                        Ship = new Ship(
+                            fields[6],
+                            fields[7],
+                            fields[8],
+                            fields[9]
+                            )
+                    };
+
+                    passengers.Add(passenger);
+
+                    // debug stuff
+                    // Console.WriteLine(passenger.ToString());
+                }
+            }
+
+            return passengers;
         }
     }
 }
